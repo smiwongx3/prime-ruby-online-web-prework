b933f3c7cb71d7f564ca9b98ae237afbec50c17c
Samantha Wong <smiwong920@gmail.com>
2019-02-01 21:02:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(n)
+  if n < 0 or n == 0 or n == 1
+    return false
+  else
+    (2..n-1).to_a.all? do |possible_factor|
+      n % possible_factor != 0
+    end
+  end
+end
