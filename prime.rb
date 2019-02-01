025bfe4f33b0d573125bb0c16d4c0ada18c1e553
Samantha Wong <smiwong920@gmail.com>
2019-02-01 21:02:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(n)
-require 'prime'
-  if Prime.prime?(n)  
-    "true"
-  else 
-    "false" 
-  end 
-end 
+  if n < 0 or n == 0 or n == 1
+    return false
+  else
+    (2..n-1).to_a.all? do |possible_factor|
+      n % possible_factor != 0
+    end
+  end
+end
